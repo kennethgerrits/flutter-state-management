927644026eefc9068174915b2178b94db68e2e12
Kenneth Gerrits <kennethgerrits@gmail.com>
2022-02-09 13:10:09 +0100

    apply CS

--- state_management/lib/views/product_page.dart
+++ state_management/lib/views/product_page.dart
@@ -3,33 +3,36 @@ import 'package:state_management/models/product.dart';
 import 'package:state_management/views/widgets/rating_box.dart';
 
 class ProductPage extends StatelessWidget {
-  ProductPage({Key? key, required this.item}) : super(key: key);
+  const ProductPage({Key? key, required this.item}) : super(key: key);
   final Product item;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text(this.item.name),
+        title: Text(item.name),
       ),
       body: Center(
         child: Container(
-          padding: EdgeInsets.all(0),
+          padding: const EdgeInsets.all(0),
           child: Column(
               mainAxisAlignment: MainAxisAlignment.start,
               crossAxisAlignment: CrossAxisAlignment.start,
               children: <Widget>[
-                Image.asset("assets/" + this.item.image),
+                Image.asset("assets/" + item.image),
                 Expanded(
                     child: Container(
-                        padding: EdgeInsets.all(5),
+                        padding: const EdgeInsets.all(5),
                         child: Column(
                           mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                           children: <Widget>[
-                            Text(this.item.name,
-                                style: TextStyle(fontWeight: FontWeight.bold)),
-                            Text(this.item.description),
-                            Text("Price: " + this.item.price.toString()),
+                            Text(
+                              item.name,
+                              style:
+                                  const TextStyle(fontWeight: FontWeight.bold),
+                            ),
+                            Text(item.description),
+                            Text("Price: " + item.price.toString()),
                             RatingBox(),
                           ],
                         )))
