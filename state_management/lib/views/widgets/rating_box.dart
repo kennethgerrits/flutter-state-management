6e502104032ed550f998a5aa8a9903bc02fbd356
Kenneth Gerrits <kennethgerrits@gmail.com>
2022-02-10 09:26:56 +0100

    use loop to remove redundant code

--- state_management/lib/views/widgets/rating_box.dart
+++ state_management/lib/views/widgets/rating_box.dart
@@ -14,58 +14,25 @@ class RatingBox extends StatelessWidget {
       mainAxisAlignment: MainAxisAlignment.end,
       crossAxisAlignment: CrossAxisAlignment.end,
       mainAxisSize: MainAxisSize.max,
-      children: <Widget>[
-        Container(
-          padding: const EdgeInsets.all(0),
-          child: IconButton(
-            icon: (_rating >= 1
-                ? Icon(
-                    Icons.star,
-                    size: _size,
-                  )
-                : Icon(
-                    Icons.star_border,
-                    size: _size,
-                  )),
-            color: Colors.red[500],
-            onPressed: () => context.read<Rating>().setRating(1),
-            iconSize: _size,
+      children: [
+        for (int i = 1; i <= 3; i++)
+          Container(
+            padding: const EdgeInsets.all(0),
+            child: IconButton(
+              icon: (_rating >= i
+                  ? Icon(
+                      Icons.star,
+                      size: _size,
+                    )
+                  : Icon(
+                      Icons.star_border,
+                      size: _size,
+                    )),
+              color: Colors.red[500],
+              onPressed: () => context.read<Rating>().setRating(i),
+              iconSize: _size,
+            ),
           ),
-        ),
-        Container(
-          padding: const EdgeInsets.all(0),
-          child: IconButton(
-            icon: (_rating >= 2
-                ? Icon(
-                    Icons.star,
-                    size: _size,
-                  )
-                : Icon(
-                    Icons.star_border,
-                    size: _size,
-                  )),
-            color: Colors.red[500],
-            onPressed: () => context.read<Rating>().setRating(2),
-            iconSize: _size,
-          ),
-        ),
-        Container(
-          padding: const EdgeInsets.all(0),
-          child: IconButton(
-            icon: (_rating >= 3
-                ? Icon(
-                    Icons.star,
-                    size: _size,
-                  )
-                : Icon(
-                    Icons.star_border,
-                    size: _size,
-                  )),
-            color: Colors.red[500],
-            onPressed: () => context.read<Rating>().setRating(3),
-            iconSize: _size,
-          ),
-        ),
       ],
     );
   }
